7ab426298643a8ad040fa0caa9187b7a512a4baa
Kirstin Reid <kirstinreid_@hotmail.co.uk>
2020-04-07 23:41:47 +0200

    navigation working

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,14 @@
 
-
 requirejs.config({
     baseUrl: "../node_modules/",
     paths: {
         d3js: 'd3/dist/d3.min',
         neo4j: 'neo4j-driver/lib/browser/neo4j-web.min',
-        search: '../src/js/search.js',
-        soundcloud: '../src/js/soundcloud.js',
-        graph: '../src/js/queryexecuter.js',
-        actions: '../src/js/actions.js',
-        run: '../src/js/run.js'
+        search: '../src/js/search',
+        soundcloud: '../src/js/soundcloud',
+        graph: '../src/js/graph',
+        actions: '../src/js/actions',
+        run: '../src/js/run'
     }
 });
 
--- src/js/soundcloud.js
+++ src/js/soundcloud.js
@@ -27,36 +27,13 @@ define(function () {
         });
     },
 
-    likesToUsers: function(arr, callback) {
-        var data = [];
-        for (var i = 0; i < arr.length; i++) {
-            data.push(arr[i].user);
-        }
-        callback(data);
-    },
-
-    getFinalData: function(a, b, callback) {
-      var data = [];
-        data = a.concat(b);
-        for (var i = 0; i < data.length; i++) {
-          data[i].ranking = 1;
-        }
-      callback(data);
-    },
-
-    getNewFinalData: function(a, b, callback) {
-      var data=[];
-      data = a.concat(b);
-      callback(data);
-    },
-
     unique: function(array, callback) {
       let data = [];
 
       const count = array =>
         array.reduce((a, b) => ({ ...a,
           [b]: (a[b] || 0) + 1
-        }), {}) // don't forget to initialize the accumulator
+        }), {})
 
       var artistsRanking = count(array.map((item) => item.id ));
 
@@ -72,31 +49,6 @@ define(function () {
       callback(data);
     },
 
-    removeIfTooMany: function(arr, callback){
-      arr.sort(sortOn("followers_count"));
-      arr.sort(sortOn("reposts_count"));
-      arr.sort(sortOn("ranking"));
-      while(arr.length > 70){
-          arr.splice(1,1);
-      }
-      callback(arr);
-    },
-
-    concat: function(arr, final, callback) {
-      for (var p = 0; p < arr.length; p++) {
-          final.artists[p] = arr[p];
-      }
-      callback(final.artists);
-    },
-
-    assignArray: function(b, callback){
-      usersList = [];
-      for(var i = 0; i < b.length; i++){
-        usersList[i] = b[i];
-      }
-      usersList.sort(sortOn("username"));
-      callback(usersList);
-    },
 
     getPagePlaylist: function(){
       var pagePlaylist = [];
@@ -147,46 +99,6 @@ define(function () {
             }
         });
     },
-                // console.log(response.tracks.items[0].preview_url);
-    checkIfFollowing: function(user,curr){
-
-      for (var u = 0; u < alreadyfollowing.length; u++) {
-        if(curr.id === alreadyfollowing[u].id){
-          $("#followbtn").attr("src","/images/following.png");
-          console.log("already following user");
-          following = true;
-          break;
-        } else{
-          $("#followbtn").attr("src","/images/follow.png");
-        }
-      }
-    },
-
-    // function followUnfollowUser(user,curr){
-    //       if(following = true){
-    //         unfollowUser(user,curr);
-    //       }
-    //
-    //       if(following = false){
-  //         followUser(user,curr);
-  //       }
-  //   }
-  //
-  //       function unfollowUser(user,curr){
-  //         SC.connect().then(function() {
-  //           SC.put('/me/followings/' + curr.id).then(function(){
-  //             following = false;
-  //             return SC.delete('/me/followings/' + curr.id)
-  //           }).then(function(user){
-  //               console.log('You unfollowed ' + user.username);
-  //             }).catch(function(error){
-  //               alert('Error: ' + error.message);
-  //             });
-  //         });
-  //         $("followbtn").attr("src","/images/follow.png");
-  //       }
-
-
 
     followUser: function(user,curr){
       SC.connect().then(function() {
